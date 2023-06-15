66b7a7f9d4ab3a54c604e9827dae49dfbb5f7f97
Tiago Rafael <tiagorafael777@gmail.com>
2023-06-15 10:05:33 -0400

    feat - e-commerce api day 01

---
+++ app.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+require("express-async-errors");
+
+// express
+const express = require("express");
+const app = express();
+
+// rest of the packages
+const morgan = require("morgan");
+
+// database
+const connectDB = require("./db/connect");
+
+// routers
+const authRouter = require("./routes/authRoutes");
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("e-commerce api");
+});
+
+app.use("/api/v1/auth", authRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
